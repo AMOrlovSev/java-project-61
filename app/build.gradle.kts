50280331319d1489e59e49386aa170b260eded82
Artem Orlov <AMOrlov.sev@gmail.com>
2025-01-11 21:58:54 +0300

    step2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,20 @@
 plugins {
     id("java")
     id("com.github.ben-manes.versions") version "0.51.0"
+    application
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+//application {
+//    // Входная точка
+//    mainClass.set("hexlet.code.App")
+//}
 
 repositories {
     mavenCentral()
